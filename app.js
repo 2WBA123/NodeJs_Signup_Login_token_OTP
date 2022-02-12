78aea63b08e3156bd7e3621c90020a39aeec164e
2WBA123 <wahabadil433@gmail.com>
2022-02-12 20:23:16 +0500

    commitAll

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express'); //Import the express dependency
+require("dotenv").config();
+require("./config/database").connect();
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+app.use(express.json());
 
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
+const port = 5000;                  //Save the port number where your server will be listening
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
